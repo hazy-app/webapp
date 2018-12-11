d867461d9d64c488ce69e04164286dc0486f71ce
Nainemom <nainemom@gmail.com>
2018-12-11 19:46:13 +0330

    create single message page

---
+++ pages/_username/messages/_message.vue
@@ -0,0 +1,81 @@
+<template>
+  <fvMain>
+    <fvContent>
+      <appHeader
+        v-if="!isMine"
+        :inbox="$store.state.parsedToken.username"
+        :login="!$store.state.parsedToken.username"
+        :logout="$store.state.parsedToken.username"
+        :home="true">
+        Message for @{{ $route.params.username }}
+      </appHeader>
+
+      <appInnerContent sm>
+
+        <div class="fv-margin-top fv-flex">
+          <div class="fv-margin-end-sm">
+            <appAccountLink 
+              size="3rem"/>
+          </div>
+          <div class="fv-border fv-shadow fv-radius fv-grow">
+            <p 
+              :style="{'direction': $calcDirection(message.text)}" 
+              class="fv-padding-sm fv-font-lg message-text">{{ message.text }}</p>
+            <small class="fv-flex fv-padding-sm fv-margin-top fv-padding-top">
+              <div class="fv-grow" />
+              <div 
+                :title="message.create_date | dateReadable" 
+                class="fv-margin-end fv-hidden-xs">
+                <span class="fa fa-text-gray">
+                  <i class="fa fa-calendar" /> {{ message.create_date | dateFromNow }}
+                </span>
+              </div>
+            </small>
+          </div>
+        </div>
+      </appInnerContent>
+    </fvContent>
+  </fvMain>
+</template>
+
+<script>
+import copy from 'clipboard-copy'
+
+export default {
+  data() {
+    return {
+      isMine: false,
+      message: {}
+    }
+  },
+  head() {
+    return {
+      title: 'Hazy',
+      meta: [
+        {
+          property: 'twitter:description',
+          content: `Look at anonymous message sent for @${
+            this.$route.params.username
+          }!`
+        }
+      ]
+    }
+  },
+  async asyncData({ params, query, store, $axios, redirect }) {
+    const ret = {}
+    try {
+      ret.message = await $axios.$get(
+        `${process.env.BASE_URL}/users/${params.username}/messages/${
+          params.message
+        }`
+      )
+    } catch (e) {
+      throw {
+        statusCode: 404,
+        message: 'Message not found!'
+      }
+    }
+    return ret
+  }
+}
+</script>
