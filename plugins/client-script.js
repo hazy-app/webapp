be7abb6d41f28d017be81f4f6eaa50053f1b284b
Nainemom <nainemom@gmail.com>
2018-12-11 19:46:46 +0330

    import only used part of firebase

--- plugins/client-script.js
+++ plugins/client-script.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/messaging'
 
 export default async ({ store, app: { $alerts, $eventBus } }) => {
   if ('Notification' in window) {
