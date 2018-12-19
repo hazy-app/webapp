bc451d4c7f77eed0e541864f656d7b9bf43af195
Nainemom <nainemom@gmail.com>
2018-12-20 02:15:03 +0330

    remove base padding from continer

--- components/appInnerContent.vue
+++ components/appInnerContent.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="fv-row">
+  <div class="fv-row fv-no-padding">
     <div class="fv-grow fv-hidden-xs" />
     <section 
       v-if="typeof $attrs.xs !== 'undefined'" 
-      class="fv-col-xs-12 fv-col-sm-10 fv-col-md-6 fv-col-lg-4">
+      class="fv-col-xs-12 fv-col-sm-10 fv-col-md-6 fv-col-lg-4 fv-no-padding">
       <slot/>
     </section>
     <section
       v-else-if="typeof $attrs.sm !== 'undefined'" 
-      class="fv-col-xs-12 fv-col-sm-10 fv-col-md-8 fv-col-lg-6">
+      class="fv-col-xs-12 fv-col-sm-10 fv-col-md-8 fv-col-lg-6 fv-no-padding">
       <slot/>
     </section>
     <section 
@@ -18,7 +18,7 @@
     </section>
     <section 
       v-else 
-      class="fv-col-xs-12 fv-col-md-10 fv-col-lg-10 fv-col-xl-8">
+      class="fv-col-xs-12 fv-col-md-10 fv-col-lg-10 fv-col-xl-8 fv-no-padding">
       <slot/>
     </section>
     <div class="fv-grow fv-hidden-xs" />
