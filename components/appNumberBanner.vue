369b64976ce3fd40577cb697e12cefd3f88d23e9
Nainemom <nainemom@gmail.com>
2019-02-26 01:56:07 +0330

    update available chars

--- components/appNumberBanner.vue
+++ components/appNumberBanner.vue
@@ -27,7 +27,7 @@ export default {
   methods: {
     randomStr(len = 1) {
       let ret = ''
-      const availableChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '%', '$', '#']
+      const availableChars = ['@', '&', '%', '$', '#']
       const gen = () =>
         availableChars[Math.floor(Math.random() * availableChars.length)]
       for (let i = 0; i < len; i++) {
