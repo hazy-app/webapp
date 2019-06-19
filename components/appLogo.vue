6c7f7ca747c39fa2d02025803d5a1eb33122eb21
Nainemom <nainemom@gmail.com>
2019-06-19 18:46:27 +0430

    create appLogo component

---
+++ components/appLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <img 
+    class="logoz"
+    src="/hazy.svg" 
+    alt="Hazy" >
+</template>
+
+
+<style lang="scss" scoped>
+.logoz {
+  vertical-align: middle;
+  height: 4rem;
+}
+</style>
