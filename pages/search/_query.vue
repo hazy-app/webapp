bdb5afd2a6f08c23737a826198623ad93191c3bf
Nainemom <nainemom@gmail.com>
2019-06-26 18:05:09 +0430

    create search page

---
+++ pages/search/_query.vue
@@ -0,0 +1,74 @@
+<template>
+  <fvMain>
+    <fvContent>
+      <!-- If it is not mine -->
+      <appHeader>
+        <template slot="title"> Hazy </template>
+        <template slot="description"> Search result for <i>{{ $route.params.query }}</i> </template>
+      </appHeader>
+
+      <appInnerContent 
+        sm 
+        class="fv-padding-sm">
+        <br>
+        <p
+          v-if="users.length === 0"
+          class="fv-text-center">
+          <i class="fa fa-circle-o" /> There is nothing to show :)
+        </p>
+        <appUserCard 
+          v-for="user in users" 
+          :user="user" 
+          :key="user.username"
+          class="fv-margin-bottom" />
+      </appInnerContent>
+
+    </fvContent>
+  </fvMain>
+</template>
+
+<script>
+import copy from 'clipboard-copy'
+import appAccountLink from '~/components/appAccountLink.vue'
+import appUserCard from '~/components/appUserCard.vue'
+
+export default {
+  components: {
+    appAccountLink,
+    appUserCard
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  head() {
+    return {
+      title: 'Hazy',
+      meta: [
+        {
+          property: 'twitter:description',
+          content: `Searching for ${this.$route.params.query}`
+        }
+      ]
+    }
+  },
+  async asyncData({ params, query, store, $axios, redirect }) {
+    const ret = {}
+    try {
+      ret.users = await $axios.$get(
+        `${process.env.BASE_URL}/search?q=${params.query}`
+      )
+    } catch (e) {
+      ret.users = []
+    }
+    return ret
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.profile-container {
+  margin-bottom: -4rem;
+}
+</style>
