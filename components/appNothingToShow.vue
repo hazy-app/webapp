401110566c17d567ef903b6b4e0b02db6497e9a0
Amir Momenian <nainemom@gmail.com>
2019-06-27 01:31:33 +0430

    create nothingToShow component

---
+++ components/appNothingToShow.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fv-text-center app-nothing-to-show fv-border fv-shadow fv-radius fv-padding">
+    <i class="fa fa-frown-o" /> 
+    <div> Nothing to show! </div>
+    <p class="fv-text-light"> This isn't 404 error, just there is no related items yet. </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app-nothing-to-show {
+  & > i {
+    font-size: 5em;
+  }
+  & > div {
+    font-size: 2em;
+    font-weight: bold;
+  }
+}
+</style>
