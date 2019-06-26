45f52a4321590ee3a3c7ed02f126e3e2470a6dce
Amir Momenian <nainemom@gmail.com>
2019-06-27 01:33:13 +0430

    use el with padding instead of br

--- pages/register.vue
+++ pages/register.vue
@@ -8,7 +8,7 @@
       <appInnerContent 
         xs 
         class="fv-padding-sm">
-        <br>
+        <div class="fv-padding-sm" />
         <fvForm 
           method="POST"
           class="fv-border fv-radius fv-shadow fv-row" 
