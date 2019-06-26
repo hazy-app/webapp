61f6def8548dd5f7324d4c9f0e7b830d7346ce12
Nainemom <nainemom@gmail.com>
2019-06-26 18:09:26 +0430

    update class-name

--- components/appUserCard.vue
+++ components/appUserCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-message fv-border fv-radius fv-shadow">
+  <div class="app-user-card fv-border fv-radius fv-shadow">
     <div class="fv-padding">
       <nuxt-link :to="'/' + user.username">
         <h3 
@@ -33,31 +33,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.app-message {
-  overflow: hidden;
-
-  & .header {
-    // background: #f7f7f7;
-    user-select: none;
-    display: flex;
-    flex-direction: row;
-
-    & .fv-switch {
-      top: -0.35em;
-    }
-  }
-
-  // & .reply-body {
-  //   background: rgba(0, 0, 0, 0.01);
-  // }
-
-  // & .highlight > span {
-  //   max-width: 60%;
-  //   margin: 0 20%;
-  //   height: 1px;
-  //   border-top: dotted 2px #e0e0e0;
-  // }
-}
-</style>
