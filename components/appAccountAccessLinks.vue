d86e0a3da910fa73700bcab36b39fa0e3f9adb99
Amir Momenian <nainemom@gmail.com>
2019-07-03 00:42:35 +0430

    add profile header

--- components/appAccountAccessLinks.vue
+++ components/appAccountAccessLinks.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
-    <div class="fv-row">
+    <div class="fv-border-bottom fv-padding-sm fv-text-center fv-text-light fv-font-lg">
+      <h2> <i 
+        :class="username === 'anonymous' ? 'fa-user-secret' : 'fa-user'" 
+        class="fa" /> </h2>
+      <p> @{{ username }} </p>
+    </div>
+    <div 
+      v-if="username !== 'anonymous'" 
+      class="fv-row">
       <div class="fv-col">
         <nuxt-link 
           :to="'/' + username" 
           exact-active-class="fv-disabled"
-          class="fv-button fv-block"> <i class="fa fa-send" /> Send Message {{ mention ? 'to ' + mentionText : '' }} </nuxt-link>
+          class="fv-button fv-block"> <i class="fa fa-send" /> Send Message </nuxt-link>
       </div>
       <div class="fv-col">
         <nuxt-link 
           :to="'/' + username + '/messages'" 
           exact-active-class="fv-disabled"
-          class="fv-button fv-block"> <i class="fa fa-inbox" /> {{ mentionText }} Inbox </nuxt-link>
+          class="fv-button fv-block"> <i class="fa fa-inbox" /> Inbox </nuxt-link>
       </div>
       <div class="fv-col">
         <nuxt-link 
           :to="'/' + username + '/polls'" 
           exact-active-class="fv-disabled"
-          class="fv-button fv-block"> <i class="fa fa-check-circle-o" /> {{ mentionText }} Polls </nuxt-link>
+          class="fv-button fv-block"> <i class="fa fa-check-circle-o" /> Polls </nuxt-link>
       </div>
+    </div>
+    <div 
+      v-else 
+      class="fv-padding">
+      We don't have any data from this person.
     </div>
   </div>
 </template>
@@ -29,17 +42,6 @@ export default {
     username: {
       type: String,
       default: ''
-    },
-    mention: {
-      type: Boolean,
-      default: false
-    }
-  },
-  computed: {
-    mentionText() {
-      if (this.mention) {
-        return `@${this.username}`
-      }
     }
   }
 }
