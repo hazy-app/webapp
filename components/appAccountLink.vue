b247f82023d37c6c3eabb27a1f35385f81865d27
Amir Momenian <nainemom@gmail.com>
2019-07-03 00:33:32 +0430

    refactor menu usage

--- components/appAccountLink.vue
+++ components/appAccountLink.vue
@@ -1,39 +1,13 @@
 <template>
-  <span>
-    <a 
-      :class="{'non-clickable': !clickable}"
-      :tabindex="clickable ? 0 : -1"
-      class="profile-link"
-      @click="onClick">
-      @{{ username }}
-    </a>
-    <fvMenu 
-      v-if="isMounted"
-      v-model="profileMenu" 
-      class="app-menu">
-      <div class="fv-border-bottom fv-padding-sm fv-text-center fv-text-light fv-font-lg">
-        <h2> <i class="fa fa-user-circle-o" /> </h2>
-        <p> @{{ username }} </p>
-      </div>
-      <div class="fv-row">
-        <div class="fv-col">
-          <nuxt-link 
-            :to="'/' + username" 
-            class="fv-button fv-block"> <i class="fa fa-send" /> Send Message </nuxt-link>
-        </div>
-        <div class="fv-col">
-          <nuxt-link 
-            :to="'/' + username + '/messages'" 
-            class="fv-button fv-block"> <i class="fa fa-inbox" /> Messages </nuxt-link>
-        </div>
-        <div class="fv-col">
-          <nuxt-link 
-            :to="'/' + username + '/polls'" 
-            class="fv-button fv-block"> <i class="fa fa-check-circle-o" /> Polls </nuxt-link>
-        </div>
-      </div>
-    </fvMenu>
-  </span>
+  <a 
+    :class="{'non-clickable': !clickable}"
+    :tabindex="clickable ? 0 : -1"
+    class="profile-link"
+    @click="onClick">
+    <i 
+      :class="username === 'anonymous' ? 'fa-user-secret' : 'fa-user'" 
+      class="fa" /> @{{ username }}
+  </a>
 </template>
 
 <script>
@@ -48,20 +22,9 @@ export default {
       default: false
     }
   },
-  data() {
-    return {
-      profileMenu: false,
-      isMounted: false
-    }
-  },
-  mounted() {
-    this.isMounted = true
-  },
   methods: {
     onClick() {
-      if (this.clickable) {
-        this.profileMenu = !this.profileMenu
-      }
+      this.$store.commit('ui/openUserPopup', this.username)
     }
   }
 }
