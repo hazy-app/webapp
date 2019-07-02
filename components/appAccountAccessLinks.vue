1faf05e358e096860caf5acb9c1d1818e5537d41
Amir Momenian <nainemom@gmail.com>
2019-07-03 00:33:22 +0430

    create account access link component

---
+++ components/appAccountAccessLinks.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="fv-row">
+      <div class="fv-col">
+        <nuxt-link 
+          :to="'/' + username" 
+          exact-active-class="fv-disabled"
+          class="fv-button fv-block"> <i class="fa fa-send" /> Send Message {{ mention ? 'to ' + mentionText : '' }} </nuxt-link>
+      </div>
+      <div class="fv-col">
+        <nuxt-link 
+          :to="'/' + username + '/messages'" 
+          exact-active-class="fv-disabled"
+          class="fv-button fv-block"> <i class="fa fa-inbox" /> {{ mentionText }} Inbox </nuxt-link>
+      </div>
+      <div class="fv-col">
+        <nuxt-link 
+          :to="'/' + username + '/polls'" 
+          exact-active-class="fv-disabled"
+          class="fv-button fv-block"> <i class="fa fa-check-circle-o" /> {{ mentionText }} Polls </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      default: ''
+    },
+    mention: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    mentionText() {
+      if (this.mention) {
+        return `@${this.username}`
+      }
+    }
+  }
+}
+</script>
