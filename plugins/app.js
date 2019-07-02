f334af3f7f10885234d56f631188767eb531392a
Nainemom <nainemom@gmail.com>
2019-07-02 14:27:26 +0430

    use js-sha256

--- plugins/app.js
+++ plugins/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import timeago from 'timeago.js'
 import Fingerprint from 'fingerprintjs2'
 import Modela from 'modela'
-
+import sha256 from 'js-sha256'
 import appAlerts from '~/components/appAlerts.vue'
 import appHeader from '~/components/appHeader.vue'
 import appInnerContent from '~/components/appInnerContent.vue'
@@ -52,16 +52,6 @@ export default ({ app, store, router }, inject) => {
   })
 
   inject('getFingerprint', async () => {
-    //https://stackoverflow.com/a/7616484
-    function hashCode(str) {
-      return str
-        .split('')
-        .reduce(
-          (prevHash, currVal) =>
-            ((prevHash << 5) - prevHash + currVal.charCodeAt(0)) | 0,
-          0
-        )
-    }
     return new Promise((resolve, reject) => {
       const timeout = window.requestIdleCallback
         ? window.requestIdleCallback
@@ -72,7 +62,7 @@ export default ({ app, store, router }, inject) => {
         })
           .then(data => {
             // console.log(JSON.stringify(data, '', 2))
-            resolve(hashCode(JSON.stringify(data)))
+            resolve(sha256(JSON.stringify(data)))
           })
           .catch(reject)
       })
