22cbf23861c9ac7e21840f329e5ae1fa5f44fe91
Amir Momenian <nainemom@gmail.com>
2019-07-04 01:38:26 +0430

    handle svg files

--- nuxt.config.js
+++ nuxt.config.js
@@ -190,6 +190,12 @@ const ret = {
           exclude: /(node_modules)/
         })
       }
+      const svgRule = config.module.rules.find(rule => rule.test.test('.svg'))
+      svgRule.test = /\.(png|jpe?g|gif|webp)$/
+      config.module.rules.push({
+        test: /\.svg$/,
+        loader: 'vue-svg-loader'
+      })
     }
   }
 }
