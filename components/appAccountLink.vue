bb808fcd94093dc0771ee2771ddf094720f4d8f4
Nainemom <nainemom@gmail.com>
2019-07-03 21:11:08 +0430

    make avatar bigger

--- components/appAccountLink.vue
+++ components/appAccountLink.vue
@@ -6,7 +6,7 @@
     @click="onClick">
     <appAvatar 
       :username="username" 
-      size="18px" /> <span class="fv-hidden-xs fv-hidden-sm"> {{ username }} </span>
+      size="24px" /> <span class="fv-hidden-xs fv-hidden-sm"> {{ username }} </span>
   </a>
 </template>
 
