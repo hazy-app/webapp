ea41726931c550934ff5a81511829c5c95fd7cb3
Nainemom <nainemom@gmail.com>
2019-07-06 18:14:47 +0430

    show default questions by default

--- pages/_username/index.vue
+++ pages/_username/index.vue
@@ -6,7 +6,7 @@
     <appAccountAccessLinks 
       :username="$route.params.username" 
       class="fv-border-top fv-border-start fv-border-end" />
-    <div 
+    <!-- <div 
       v-if="isMine"
       class="fv-padding fv-text-center fv-border fv-margin-bottom">
       <p> <appIcon icon="info" /> Share your profile link to your friends to receive anonymous messages! </p>
@@ -16,12 +16,34 @@
           class="fv-primary" 
           @click="copyLink"> <appIcon icon="copy" /> Copy Link </fvButton>
       </div>
+    </div> -->
+    <div
+      v-if="isMine" 
+      class="fv-padding fv-text-center fv-border fv-margin-bottom">
+      <p> <appIcon icon="info" /> Share your created questions to your friends and let them to answer it anonymously! </p>
+      <div class="fv-margin-top">
+        <nuxt-link 
+          :to="'/' + $store.state.parsedToken.username + '/questions/new'" 
+          class="fv-button fv-primary"> <appIcon icon="plus-circle" /> Create New Question </nuxt-link>
+      </div>
     </div>
-    <appMessageSender 
-      :user="$route.params.username"
-      class="fv-border" 
-      save-button
-      @sent="$router.push('/' + $route.params.username + '/messages/' + $event.uuid)"/>
+
+    <appQuestion 
+      v-for="question in questions"
+      :key="'question' + question._id" 
+      :question="question"
+      :edit-buttons="false"
+      :open-button="true"
+      :watch-as="isMine ? 'creator' : 'user'"
+      class="fv-margin-bottom" />
+    <div class="fv-text-center">
+      <fvButton 
+        v-if="hasNext && !loading" 
+        @click="loadMore">
+        <appIcon icon="more-horizontal" /> Load More
+      </fvButton>
+      <fvLoading v-if="loading" />
+    </div>
   </appInnerContent>
 </template>
 
@@ -30,18 +52,20 @@ import copy from 'clipboard-copy'
 import appAccountLink from '~/components/appAccountLink.vue'
 import appAccountAccessLinks from '@/components/appAccountAccessLinks.vue'
 import appIcon from '@/components/appIcon.vue'
+import appQuestion from '@/components/appQuestion.vue'
 
 export default {
   components: {
     appAccountLink,
     appAccountAccessLinks,
-    appIcon
+    appIcon,
+    appQuestion
   },
   data() {
     return {
       isMine: false,
       loading: false,
-      user: {},
+      questions: [],
       form: {
         text: '',
         recaptcha: false
@@ -53,41 +77,25 @@ export default {
       const direction = this.$calcDirection(str)
       this.$refs.input.$el.style.direction = direction
     },
-    async send() {
-      this.$root.$loading.start()
-      try {
-        const response = await this.$axios.$post(
-          `${process.env.BASE_URL}/users/${
-            this.$route.params.username
-          }/messages`,
-          {
-            text: this.form.text,
-            recaptcha: this.form.recaptcha
-          }
-        )
-        this.$root.$loading.finish()
-        this.$alerts.toast(
-          `Your message has been sent to ${
-            this.$route.params.username
-          } successfully!`,
-          'success'
-        )
-        this.$router.push('/')
-      } catch (e) {
-        this.form.recaptcha = false
-        this.$root.$loading.finish()
-        this.$alerts.toast(
-          (((e || {}).response || {}).data || {}).message || 'Unhandled Error!',
-          'failed'
-        )
-      }
-    },
     copyLink() {
       const url = `${document.location.protocol}//${document.location.host}/${
         this.$route.params.username
       }`
       copy(url)
       this.$alerts.toast('Link copied to clipboard!')
+    },
+    async loadMore() {
+      this.loading = true
+      this.page++
+      const response = await this.$axios.$get(
+        `${process.env.BASE_URL}/users/${
+          this.$store.state.parsedToken.username
+        }/questions?per_page=10&page=${this.page}`
+      )
+      this.questions = this.questions.concat(response.result)
+      this.hasNext = response.hasNext
+
+      this.loading = false
     }
   },
   head() {
@@ -103,6 +111,19 @@ export default {
   },
   async asyncData({ params, query, store, $axios, redirect }) {
     const ret = {}
+    ret.page = query.page ? parseInt(query.page) : 1
+    try {
+      const response = await $axios.$get(
+        `${process.env.BASE_URL}/users/${
+          params.username
+        }/questions?per_page=10&page=${ret.page}`
+      )
+      ret.hasNext = response.hasNext
+      ret.totalPages = response.totalPages
+      ret.questions = response.result
+    } catch (e) {
+      return redirect('/login')
+    }
     ret.isMine = store.state.parsedToken.username === params.username
     store.commit('ui/setHeader', {
       title: `@${params.username}`,
