7ed51120227b7cafd14a5baad2479ab4d39d28eb
Nainemom <nainemom@gmail.com>
2019-07-07 16:32:31 +0430

    add view-replies button

--- components/appQuestion.vue
+++ components/appQuestion.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-message fv-border">
+  <div class="app-message">
     <div class="fv-padding fv-flex header">
       <span>
         <appAccountLink :username="question.creator"/>
@@ -20,8 +20,15 @@
         <appIcon icon="calendar" /> {{ question.create_date | dateFromNow }}
       </span>
       <nuxt-link 
+        v-if="viewRepliesButton" 
+        :to="'/' + question.creator + '/messages?question=' + question._id" 
+        class="fv-margin-start fv-link">
+        <appIcon icon="list"/>
+        <span class="fv-hidden-xs"> View Replies </span> 
+      </nuxt-link>
+      <nuxt-link 
         v-if="openButton" 
-        :to="'/' + question.creator + '/send-message/' + question._id" 
+        :to="'/' + question.creator + '/questions/' + question._id" 
         class="fv-margin-start fv-link">
         <appIcon 
           v-if="watchAs === 'user'"  
@@ -29,60 +36,37 @@
         <span 
           v-if="watchAs === 'creator'" 
           class="fv-hidden-xs"> Open </span>
-        <span v-else> Answer </span> 
+        <span v-else> Reply </span> 
         <appIcon 
           v-if="watchAs === 'creator'" 
           icon="arrow-right"/>
       </nuxt-link>
     </div>
-    <div class="fv-padding">
+    <div class="fv-padding fv-font-lg">
       <p :style="{'direction': $calcDirection(question.text)}">
         {{ question.text }}
       </p>
     </div>
-    <fvForm 
-      v-if="false" 
-      class="fv-padding fv-border-top choices"
-      method="POST"
-      @submit="vote">
-      <fvFormElement inline>
-        <div 
-          v-for="(choice, index) in poll.choices"
-          :key="'choice' + index" 
-          class="fv-flex choice">
-          <fvCheck 
-            v-model="selectedChoice"
-            :content="index"
-            class="fv-margin-end"
-            tabindex="-1"> {{ choice }} </fvCheck>
-        </div>
-      </fvFormElement>
-      <fvFormElement class="fv-col-12">
-        <div class="fv-text-center">
-          <no-ssr>
-            <appRecaptcha v-model="recaptcha" />
-          </no-ssr>
-        </div>
-      </fvFormElement>
-      <div class="fv-flex">
-        <fvButton 
-          type="submit" 
-          class="fv-primary fv-grow">
-          <appIcon icon="check" /> Submit
-        </fvButton>
-      </div>
-    </fvForm>
+    <appMessageSender 
+      v-if="sendForm" 
+      :user="question.creator"
+      :question="question._id" 
+      class="fv-border-top"
+      save-button
+      @sent="$emit('answered', $event)"/>
   </div>
 </template>
 
 <script>
 import appAccountLink from '~/components/appAccountLink.vue'
 import appIcon from '@/components/appIcon.vue'
+import appMessageSender from '~/components/appMessageSender.vue'
 
 export default {
   components: {
     appAccountLink,
-    appIcon
+    appIcon,
+    appMessageSender
   },
   props: {
     question: {
@@ -97,6 +81,14 @@ export default {
       type: Boolean,
       default: false
     },
+    viewRepliesButton: {
+      type: Boolean,
+      default: false
+    },
+    sendForm: {
+      type: Boolean,
+      default: false
+    },
     watchAs: {
       type: String,
       validator: v => ['creator', 'user'].indexOf(v) > -1,
@@ -105,35 +97,7 @@ export default {
   },
   data() {
     return {
-      selectedChoice: undefined,
-      userFingerprint: undefined,
-      recaptcha: false,
-      infoPopupItem: undefined,
-      infoPopup: false
-    }
-  },
-  computed: {
-    totalVotes() {
-      return this.poll.answers.reduce(
-        (total, item) => (total = total + item),
-        0
-      )
-    },
-    activeChoices() {
-      return this.poll.choices
-        .map((choice, index) => {
-          return {
-            choice,
-            index
-          }
-        })
-        .filter((choice, index) => {
-          return this.poll.answers[index] > 0
-        })
-    },
-    votesPercetange() {
-      const totalVotes = this.totalVotes
-      return this.poll.answers.map(vote => (vote * 100) / totalVotes)
+      recaptcha: false
     }
   },
   methods: {
@@ -142,21 +106,8 @@ export default {
         'Are you sure you want to delete this question?'
       )
       if (check) {
-        this.$emit('remove', this.poll)
+        this.$emit('remove', this.question)
       }
-    },
-    async vote() {
-      const fingerprint = await this.$getFingerprint()
-      this.$emit('vote', {
-        choice: this.selectedChoice,
-        fingerprint,
-        recaptcha: this.recaptcha,
-        uuid: this.poll.uuid
-      })
-    },
-    toggleInfoPopup(index) {
-      this.infoPopup = !this.infoPopup
-      this.infoPopupItem = index
     }
   }
 }
