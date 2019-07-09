3934a6a012faebeaef59a01fb34e9d64c764cd37
Amir Momenian <nainemom@gmail.com>
2019-07-10 00:41:20 +0430

    create twitter card util

---
+++ utils/twitter-card.js
@@ -0,0 +1,32 @@
+// from https://stackoverflow.com/a/41602636
+export default function(
+  username = undefined,
+  objectType = undefined,
+  fullDescription = undefined,
+  title = undefined
+) {
+  return {
+    title: title || objectType,
+    meta: [
+      {
+        property: 'twitter:card',
+        content: 'summary'
+      },
+      {
+        property: 'twitter:image:src',
+        content: !!username
+          ? `${process.env.BASE_URL}/users/${username}/avatar.jpg`
+          : 'https://hazyapp.com/hazy_card.png'
+      },
+      {
+        property: 'twitter:title',
+        content: title || `${username} ${objectType} - Hazy`
+      },
+      {
+        property: 'twitter:description',
+        content:
+          fullDescription || `Look at ${objectType} by ${username} on Hazy!`
+      }
+    ]
+  }
+}
